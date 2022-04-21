25926ec3277553c25651388615446bc48d3f3b86
Çağdaş ÖZER <cagdas.1997@hotmail.com>
2022-04-21 17:29:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Çağdaş";
+            int yas = 24;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Çağdaş Özer";
+            kurs1.IzlenmeOranı = 75;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Ptyhon";
+            kurs2.Egitmen = "Yağmur Yücel";
+            kurs2.IzlenmeOranı = 65;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Java";
+            kurs3.Egitmen = "İbrahim Furkan Baran";
+            kurs3.IzlenmeOranı = 90;
+
+            //Console.WriteLine(kurs1.KursAdi + " " + kurs1.Egitmen + " " + kurs1.IzlenmeOranı);
+            //Console.WriteLine(kurs2.KursAdi + " " + kurs2.Egitmen + " " + kurs2.IzlenmeOranı);
+            //Console.WriteLine(kurs3.KursAdi + " " + kurs3.Egitmen + " " + kurs3.IzlenmeOranı);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen + " " + kurs.IzlenmeOranı + "%");
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public string Egitmen { get; set; }
+
+        public int IzlenmeOranı { get; set; }
+    }
+
+}
